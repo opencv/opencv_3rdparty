1ca0e6eb001bbb14a03f4b66e43ffb2fa146495b
Alexander Alekhin <alexander.a.alekhin@gmail.com>
2019-09-05 21:13:58 +0000

    3.4: bump wrapper version, ffmpeg 3.4.6, libvpx 1.8.0 -> 1.8.1

--- ffmpeg/download_src.sh
+++ ffmpeg/download_src.sh
@@ -22,7 +22,7 @@ mkdir -p ../build
 cd ../build
 update ffmpeg git://source.ffmpeg.org/ffmpeg.git n3.4.6
 update openh264 https://github.com/cisco/openh264.git v1.7.0
-update libvpx https://chromium.googlesource.com/webm/libvpx.git v1.8.0
+update libvpx https://chromium.googlesource.com/webm/libvpx.git v1.8.1
 )
 
 # Pack all source code / build scripts
